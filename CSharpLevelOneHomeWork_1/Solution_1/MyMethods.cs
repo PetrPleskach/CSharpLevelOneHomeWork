5796534fb5a4806ea3a17b0e4ce92dee7d82f7f7
PetrPleskach <Pleskach_petr@mail.ru>
2020-11-25 19:59:32 +0300

    Добавлена первая домашняя работа.

---
+++ CSharpLevelOneHomeWork_1/Solution_1/MyMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_1
+{
+    //6. * Создать класс с методами, которые могут пригодиться в вашей учебе(Print, Pause).
+    public class MyMethods
+    {
+        public static void Print(string msg, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine();
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+        public static void Pause(string msg = "Нажмите любую клавишу чтобы продолжить...", ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = color;            
+            Console.WriteLine(msg);
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpLevelOneHomeWork_1/Solution_5/Program5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_5
+{
+    //а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+    class Program5
+    {
+        static void Main(string[] args)
+        {
+            //б) Сделать задание, только вывод организуйте в центре экрана
+            Print("Домашние задания выполнены:", Console.WindowWidth/2, Console.WindowHeight/2);
+            Print("Пётр Плескач г.Таганрог", Console.WindowWidth / 2, Console.WindowHeight / 2 + 1);
+            Console.ReadKey();
+        }
+        //в) *Сделать задание б с использованием собственных методов (например, Print(string ms, int x,int y)
+        static void Print(string str, int x, int y)
+        {
+            Console.SetCursorPosition(x - (str.Length/2), y); ;
+            Console.WriteLine(str);
+        }
+    }
+}
